6fdd664db467d927e79af9cf89e5cad1423045fc
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-05-31 03:25:57 +0530

    setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
-
 function App() {
-
   return (
-    <>
-      
-    </>
-  )
+    <div className="h-screen">
+      <div className="h-5 bg-amber-300">
+        <h1 className="text-3xl p-5">Card</h1>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
