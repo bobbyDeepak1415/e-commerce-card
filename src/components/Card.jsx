9ab11eeda3b5632658c727d832b6f58e3fb54e53
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-05-31 16:14:58 +0530

    setup done

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,31 @@
+const Card = ({ children, className = "", variant, padding, hover = true }) => {
 
-const Card = () => {
+    
+  const baseStyles = "shadow-md rounded-lg transition-all duration-300";
+
+  const variants = {
+    default: "bg-gray-100 border border-gray-700",
+    primary: "bg-blue-100 border border-blue-300",
+    success: "bg-green-100 border border-green-300",
+    dark: "bg-gray-800 broder border-gray-600 text-white",
+  };
+
+  const hoverStyles = hover ? "hover:shadow-lg hover:-transition-y-1" : "";
+
+  const paddingStyles = {
+    none: "p-0",
+    small: "p-4",
+    medium: "p-6",
+    large: "p-8",
+  };
+
   return (
-    <div>
-      
+    <div
+      className={`${className} ${paddingStyles[padding]} ${variants[variant]} ${hoverStyles} ${baseStyles}`}
+    >
+      {children}
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
