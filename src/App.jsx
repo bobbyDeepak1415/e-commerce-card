484ddac36d386d707659c723b04de76fd267eb30
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-06-01 00:09:42 +0530

    setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import Card from "./components/Card/Card";
+import { cardsData } from "./data/cardsData";
 
 function App() {
-  return (
-    <div className="p-8 min-h-screen bg-gray-100">
-     
-    </div>
-  );
+  return <div className="p-8 min-h-screen bg-gray-100">
+<div>
+
+    {cardsData.map((card)=>(
+      <Card>{card}</Card>
+    ))}
+</div>
+  </div>;
 }
 
 export default App;
