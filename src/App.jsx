89e0d7dcadaf082b58c2ff1737c5777c9c14f677
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-05-31 18:16:14 +0530

    setup done

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,7 @@ import Card from "./components/Card/Card";
 function App() {
   return (
     <div className="p-8 min-h-screen bg-gray-100">
-      <Card variant="primary" padding="large">
-        <p>Hello</p>
-
-        <h2>This is working</h2>
-      </Card>
+     
     </div>
   );
 }
